7210d0373fd16059d2e898fa39a1f55c5a4cdf92
Bustarde <bustarde.audriel@auf.edu.ph>
2022-11-18 23:53:17 +0800

    third commit

--- pdf-5.php
+++ pdf-5.php
@@ -5,15 +5,29 @@ use Fpdf\Fpdf;
 
 $pdf = new Fpdf();
 $pdf->AddFont('Sensation');
+$pdf->AddFont('Bostana');
+$pdf->AddFont('Enigma');
+
 $pdf->AddPage();
 $pdf->SetFont('Sensation','',35);
-$pdf->Write(10,'The Lord Of The Rings');
+$pdf->Write(10,'The future belongs to those who believe in the beauty of their dreams.');
+$pdf->Ln(10);
+$pdf->Write(10,'- Eleanor Roosevelt');
 
 $pdf->Ln(20);
 
-$pdf->SetFont('Sensation','',35);
-$pdf->Write(10,'Written By John Ronald Reuel Tolkien');
+$pdf->SetFont('Bostana','',20);
+$pdf->Write(10,'It is during our darkest moments that we must focus to see the light.');
+$pdf->Ln(10);
+$pdf->Write(10,'- Aristotle');
+
+$pdf->Ln(20);
+
+$pdf->SetFont('Enigma','',17);
+$pdf->Write(10,'Never let the fear of striking out keep you from playing the game.');
+$pdf->Ln(10);
+$pdf->Write(10,'- Babe Ruth');
 $pdf->Output('D', 'pdf-5.pdf');
 
-// Run php vendor/fpdf/fpdf/src/Fpdf/makefont/makefont.php fonts/Sensation.ttf
+// Run php vendor/fpdf/fpdf/src/Fpdf/makefont/makefont.php fonts/Enigma.ttf
 // Move the generated files to the font folder (vendor/fpdf/fpdf/src/Fpdf/font/)
